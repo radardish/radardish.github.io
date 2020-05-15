b0be832d9d948dee5d8b6a96ff2a626ffdcc6208
Christoph Grotz <christophgrotz@gmail.com>
2020-05-15 19:36:23 +0200

    Updates

---
+++ precache-manifest.5d4c08f7a55b20282d83cee79b3413a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c47d31c0fc747a204dc5a6d311b385d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05a532d925957b85983f",
+    "url": "/static/css/main.e37ee6e3.chunk.css"
+  },
+  {
+    "revision": "5d12fef333977dc7a0e1",
+    "url": "/static/js/2.3682eb72.chunk.js"
+  },
+  {
+    "revision": "0ad7f904ff7f0254bb77a371a4acdb8f",
+    "url": "/static/js/2.3682eb72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05a532d925957b85983f",
+    "url": "/static/js/main.e8506c1a.chunk.js"
+  },
+  {
+    "revision": "34fae159b5a701a8b87d",
+    "url": "/static/js/runtime-main.f43068d7.js"
+  }
+]);
